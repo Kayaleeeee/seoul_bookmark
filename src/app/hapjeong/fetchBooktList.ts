f59ab00fbf9ec4c9dd1fcc37acb44510f7a3a394
Kaya Lee <gayeon71057@gmail.com>
2023-11-09 17:54:49 +0900

    합정 api에 요청에 params 추가

--- src/app/hapjeong/fetchBooktList.ts
+++ src/app/hapjeong/fetchBooktList.ts
@@ -1,7 +1,10 @@
+import { BookStatus } from "@/pages/api/hapjeong";
 import axios from "axios";
 
 export const fetchBooktList = (params: {
   index: number;
+  bookStatus?: BookStatus;
+  title?: string;
   category_nickname_key?: string;
 }) => {
   return axios.post(`${process.env.api}/hapjeong`, params);
--- src/pages/api/hapjeong.ts
+++ src/pages/api/hapjeong.ts
@@ -1,8 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-
 import axios from "axios";
-
 import { HapjeongBookType } from "@/app/types/HapjeongBookType";
+
+export enum BookStatus {
+  unavailable = 0,
+  avaliable = 1,
+}
 
 type ResponseData =
   | {
@@ -14,12 +17,33 @@ type ResponseData =
     }
   | { error: string };
 
+const parsingUrl = (params: Record<string, any>) => {
+  let url = "";
+
+  Object.keys(params).forEach((key) => {
+    if (key === "index") {
+      url += `&pageIdx=${params[key]}`;
+    }
+
+    if (key === "bookStatus") {
+      url += `&searchState=${params[key]}`;
+    }
+
+    if (key === "title") {
+      url += `&title=${params[key]}`;
+    }
+  });
+
+  return url;
+};
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<ResponseData>
 ) {
   try {
     const params = req.body;
+
     const data = await axios
       .post<{
         result: {
@@ -30,7 +54,9 @@ export default async function handler(
           pageSize: number;
         };
       }>(
-        `https://smartlib.mapo.go.kr:9525/api/book/getBookList?pageIdx=${params.index}&pageSize=18&smartlib=1&`
+        `https://smartlib.mapo.go.kr:9525/api/book/getBookList?&pageSize=18&smartlib=1${parsingUrl(
+          params
+        )}`
       )
       .then(({ data: { result } }) => {
         return result;
