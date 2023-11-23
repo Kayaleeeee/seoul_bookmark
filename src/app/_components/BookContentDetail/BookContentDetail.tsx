fda3dab8c127ff07f7b996a27095844270b38668
Kaya Lee <gayeon71057@gmail.com>
2023-11-23 10:56:51 +0900

    BookContentDetail 컴포넌트 추가

---
+++ src/app/_components/BookContentDetail/BookContentDetail.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import { Header } from "../Header";
+import { Spacer } from "../Spacer";
+import { BookStatusBadge } from "../BookStatusBadge/BookStatusBadge";
+
+type Props = {
+  author: string;
+  title: string;
+  publisher: string;
+  isAvailable: boolean;
+  imageUrl: string;
+  description: string;
+};
+export const BookContentDetail = ({
+  author,
+  title,
+  isAvailable,
+  publisher,
+  imageUrl,
+  description,
+}: Props) => {
+  return (
+    <>
+      <div className="h-20">
+        <Header>
+          <Spacer space="40px" />
+        </Header>
+      </div>
+      <div className="flex justify-center relative">
+        <div
+          className="rounded-lg shadow-md relative"
+          style={{
+            width: "180px",
+            height: "220px",
+            zIndex: 2,
+          }}
+        >
+          <Image
+            className="rounded-lg"
+            fill
+            src={imageUrl}
+            alt={"book thumbnail"}
+            unoptimized
+          />
+        </div>
+        <div className="detailBookContent">
+          <div className="text-center text-xl font-medium">{title}</div>
+          <Spacer space="1rem" />
+
+          <div className="text-center font-medium">
+            <div>{author}</div>
+            <div>{publisher}</div>
+          </div>
+          <Spacer space="1rem" />
+
+          <div className="flex justify-center">
+            <BookStatusBadge isAvailable={isAvailable} />
+          </div>
+          <Spacer space="1rem" />
+
+          <div>{description}</div>
+        </div>
+      </div>
+    </>
+  );
+};
--- src/app/hapjeong/[bookNumber]/page.tsx
+++ src/app/hapjeong/[bookNumber]/page.tsx
@@ -1,9 +1,7 @@
-import { Header } from "@app/_components/Header";
 import React from "react";
 import { fetchBookDetail } from "./fetchBookDetail";
-import Image from "next/image";
-import { Spacer } from "@app/_components/Spacer";
-import { BookStatusBadge } from "@app/_components/BookStatusBadge/BookStatusBadge";
+
+import { BookContentDetail } from "@app/_components/BookContentDetail/BookContentDetail";
 
 const HapjeongDetailPage = async ({
   params,
@@ -24,48 +22,14 @@ const HapjeongDetailPage = async ({
         minHeight: "100vh",
       }}
     >
-      <div className="h-20">
-        <Header>
-          <Spacer space="40px" />
-        </Header>
-      </div>
-      <div className="flex justify-center relative">
-        <div
-          className="rounded-lg shadow-md relative"
-          style={{
-            width: "180px",
-            height: "220px",
-            zIndex: 2,
-          }}
-        >
-          <Image
-            className="rounded-lg"
-            fill
-            src={bookDetail.image_url}
-            alt={"book thumbnail"}
-            unoptimized
-          />
-        </div>
-        <div className="detailBookContent">
-          <div className="text-center text-xl font-medium">
-            {bookDetail.title}
-          </div>
-          <Spacer space="1rem" />
-
-          <div className="text-center font-medium">
-            <div>{bookDetail.author}</div>
-            <div>{bookDetail.publisher}</div>
-          </div>
-          <Spacer space="1rem" />
-
-          <div className="flex justify-center">
-            <BookStatusBadge isAvailable={bookDetail.state_nm !== "대출중"} />
-          </div>
-          <Spacer space="1rem" />
-
-          <div>{bookDetail.description}</div>
-        </div>
-      </div>
+      <BookContentDetail
+        author={bookDetail.author}
+        title={bookDetail.title}
+        publisher={bookDetail.publisher}
+        imageUrl={bookDetail.image_url}
+        description={bookDetail.description}
+        isAvailable={bookDetail.state_nm !== "대출중"}
+      />
     </div>
   );
 };
