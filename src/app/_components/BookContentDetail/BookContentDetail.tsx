df6087ee57930259e87fd99d528984add1aa8667
Kaya Lee <gayeon71057@gmail.com>
2023-12-18 14:27:14 +0900

    book detail ui 변경

--- src/app/_components/BookContentDetail/BookContentDetail.tsx
+++ src/app/_components/BookContentDetail/BookContentDetail.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Image from "next/image";
 import { Header } from "../Header";
 import { Spacer } from "../Spacer";
+
 import { BookStatusBadge } from "../BookStatusBadge/BookStatusBadge";
 
 type Props = {
@@ -22,43 +25,48 @@ export const BookContentDetail = ({
   return (
     <>
       <div className="h-20">
-        <Header>
-          <Spacer space="40px" />
-        </Header>
+        <Header />
       </div>
-      <div className="flex justify-center relative">
-        <div
-          className="rounded-lg shadow-md relative"
-          style={{
-            width: "180px",
-            height: "250px",
-            zIndex: 2,
-          }}
-        >
-          <Image
-            className="rounded-lg"
-            fill
-            src={imageUrl}
-            alt={"book thumbnail"}
-            unoptimized
-          />
+      <div className="flex-col justify-center bg-white border-y-2 border-black">
+        <div className="flex flex-row content-center border-b-2 border-black">
+          <div
+            className="relative border-r-2 shrink-0 border-black"
+            style={{
+              width: "180px",
+              height: "250px",
+            }}
+          >
+            <Image fill src={imageUrl} alt={"book thumbnail"} unoptimized />
+          </div>
+          <div className="px-3 py-2">
+            <div className="text-xl font-medium mb-3">{title}</div>
+            <div className="font-medium">
+              <div className="mb-2">{author}</div>
+              <div>{publisher}</div>
+            </div>
+            <Spacer space="1rem" />
+
+            <div className="flex">
+              <BookStatusBadge isAvailable={isAvailable} />
+            </div>
+          </div>
         </div>
-        <div className="detailBookContent">
-          <div className="text-center text-xl font-medium">{title}</div>
-          <Spacer space="1rem" />
 
-          <div className="text-center font-medium">
-            <div>{author}</div>
-            <div>{publisher}</div>
+        <div className="p-4">
+          <Spacer space="1rem" />
+          <div className="text-lg">{description}</div>
+          <Spacer space="2rem" />
+        </div>
+
+        <Spacer space="1rem" />
+
+        <div className="flex flex-row border-t-2 border-black ">
+          <div className="flex-1 text-lg p-4 font-bold border-r border-black text-center">
+            북마크 {isAvailable ? "저장됨" : "저장하기"}
           </div>
-          <Spacer space="1rem" />
-
-          <div className="flex justify-center">
-            <BookStatusBadge isAvailable={isAvailable} />
+          <div className="flex-1 text-lg p-4 font-bold text-center">
+            별점 남기기
           </div>
-          <Spacer space="1rem" />
-
-          <div>{description}</div>
         </div>
       </div>
     </>
