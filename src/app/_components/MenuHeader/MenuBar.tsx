31c4f9a77e3aa54a9145ae3cb938784e24502675
Kaya Lee <gayeon71057@gmail.com>
2023-12-25 00:57:41 +0900

    메뉴바 login 분기 추가

--- src/app/_components/MenuHeader/MenuBar.tsx
+++ src/app/_components/MenuHeader/MenuBar.tsx
@@ -1,20 +1,56 @@
-import Link from "next/link";
+"use client";
+
+import { useEffect, useMemo, useState } from "react";
 import "./menubar.css";
 import { useRouter } from "next/navigation";
+import { getIsLoginedFromClient } from "@app/lib/getIsLoginedFromClient";
 
 type Props = {
   closeMenu: () => void;
 };
 
 export const MenuBar = ({ closeMenu }: Props) => {
+  const [isLogined, setIsLogined] = useState<boolean>(false);
   const router = useRouter();
+
+  useEffect(() => {
+    getIsLoginedFromClient()
+      .then(() => setIsLogined(true))
+      .catch(() => setIsLogined(false));
+  }, []);
+
+  const menuList = useMemo(() => {
+    if (isLogined) {
+      return [
+        { title: "홈", path: "/" },
+        { title: "마이페이지", path: "/mypage" },
+        { title: "북마크", path: "/" },
+      ];
+    }
+    return [
+      { title: "홈", path: "/" },
+      { title: "로그인", path: "/login" },
+    ];
+  }, [isLogined]);
+
   return (
     <div>
       <div className="overlay" onClick={closeMenu} />
       <div className="menubar">
-        <div className="row" onClick={() => router.push("/mypage")}>
-          마이페이지
-        </div>
+        {menuList.map((menu) => {
+          return (
+            <div
+              key={menu.title}
+              className="row"
+              onClick={() => {
+                router.push(menu.path);
+                closeMenu();
+              }}
+            >
+              {menu.title}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
