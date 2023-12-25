2571b451c0dd2c4e86d085025a4210571ae92b5c
Kaya Lee <gayeon71057@gmail.com>
2023-12-25 17:34:21 +0900

    북마크 토글 usecase 추가

---
+++ src/app/_usecases/bookmark/useCase_toggle_bookmark.ts
@@ -0,0 +1,42 @@
+import { apiClient } from "@app/lib/api/apiClient";
+import { getIsLoginedFromClient } from "@app/lib/getIsLoginedFromClient";
+import { BookType } from "@app/types/Bookmark/BookType";
+import { useRouter } from "next/navigation";
+import { Dispatch, SetStateAction, useCallback } from "react";
+import { toast } from "react-toastify";
+
+export const useCase_toggle_bookmark = (
+  setIsSaved: Dispatch<SetStateAction<boolean>>
+) => {
+  const router = useRouter();
+
+  return useCallback(
+    async (book: BookType, isSaved: boolean) => {
+      const isLogined = await getIsLoginedFromClient();
+
+      if (!isLogined) {
+        router.push("/login");
+        return;
+      }
+
+      if (isSaved) {
+        try {
+          await apiClient.delete(`/user/bookmark/${book.id}`);
+          setIsSaved(false);
+          toast.success("북마크에서 제거했습니다 ✂️");
+        } catch {
+          toast.error("북마크 삭제에 실패했습니다 🥺");
+        }
+      } else {
+        try {
+          await apiClient.post("/user/bookmark", { book });
+          setIsSaved(true);
+          toast.success("북마크에 추가되었습니다 📚");
+        } catch {
+          toast.error("북마크 추가에 실패했습니다 🥺");
+        }
+      }
+    },
+    [setIsSaved]
+  );
+};
